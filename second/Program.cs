4551b4d9fc80cfe12445adac650f49fcfc06ddca
Yasha <yashasidin333@yandex.ru>
2023-02-23 21:35:07 +0300

    Add second

---
+++ second/Program.cs
@@ -0,0 +1,12 @@
+﻿// Квадрат введенного числа
+Console.Write("Введите число, которое хотите возвести в квадрат: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int sqr = number * number;
+
+Console.WriteLine($"Квадрат числа = {sqr}");
+
+
+
+
